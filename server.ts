ab7daf95e0fc8e78b0a584516843ec82380d8f9a
DionisioPorto16 <dionisioporto16@gmail.com>
2026-03-12 02:42:33 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT: number = Number(process.env.PORT) || 3000;
 
 /* ---------------- OPERACOES ---------------- */
 
